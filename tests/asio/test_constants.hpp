0d4a558874df421c1605f141393a2651fd5f343d
Foxyman-sama <dimalatoho1448@gmail.com>
2024-08-13 15:56:44 +0300

    Disabled tests

--- tests/asio/test_constants.hpp
+++ tests/asio/test_constants.hpp
@@ -1,16 +1,8 @@
 #ifndef TEST_CONSTANTS_HPP
 #define TEST_CONSTANTS_HPP
 
-#include <climits>
-#include <random>
+#include <string>
 #include <string_view>
-
-inline const unsigned short generate_random_port() noexcept {
-  auto rd { std::random_device {} };
-  auto seed { std::mt19937 { rd() } };
-  auto range { std::uniform_int_distribution<unsigned short>(0, USHRT_MAX) };
-  return range(seed);
-}
 
 constexpr auto test_ip { std::string_view { "127.0.0.1" } };
 inline const auto test_port { 9090 };
